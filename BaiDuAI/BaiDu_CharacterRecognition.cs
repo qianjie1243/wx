2b1ca4417f12289b889ae347462b299a83055be3
瞎 <369841603@qq.com>
2019-02-15 21:28:19 +0800

    百度AI

---
+++ BaiDuAI/BaiDu_CharacterRecognition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiDuAI
+{
+    /// <summary>
+    /// 百度AI文字识别
+    /// </summary>
+   public  class BaiDu_CharacterRecognition
+    {
+    }
+}
---
+++ BaiDuAI/BaiDu_ImageRecognition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiDuAI
+{
+    /// <summary>
+    /// 百度图像识别
+    /// </summary>
+   public  class BaiDu_ImageRecognition
+    {
+    }
+}
