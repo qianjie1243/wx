3605e6742e392cb43317aec65983bfc385d6973e
BB <369841603@qq.com>
2021-01-07 22:21:26 +0800

    修改 DockingController 异常问题，添加上传手机端上传图片（需要在HTML中配置appid 与  授权 uri）

---
+++ webFront/API_V1.1/MobileApiController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using webFront.Models;
+using WxTenpay;
+using Common;
+using System.Web.Mvc;
+
+namespace webFront.API_V1._1
+{
+    /// <summary>
+    /// 手机端接口
+    /// </summary>
+    public class MobileApiController : BaseController
+    {
+        #region 业务数据
+        WeChatPayment wxpay = new WeChatPayment();
+        WechatPublic wxpu = new WechatPublic();
+        #endregion
+
+        #region 获取微信配置
+        /// <summary>
+        /// 获取微信配置
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public object GetConfig(string url)
+        {
+            try
+            {
+                string _url = Base64.DecodeBase64(url);
+                var data = wxpu.GetWxConfig(_url);
+                return Success(data);
+            }
+            catch (Exception ex)
+            {
+                return ErrorLog(ex.ToString(), "微信前端测试接口");
+            }
+
+        }
+
+        #endregion
+
+        #region 获取基本信息
+        /// <summary>
+        /// 获取基本信息
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public object GetInformation(string code)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(code))
+                    return Redirect(wxpu.GetAuthorizationUrl(""));
+                var data = wxpu.GetOpenid(code);
+                return Success(data);
+            }
+            catch (Exception ex)
+            {
+                return ErrorLog(ex.ToString(), "微信前端测试接口");
+            }
+
+        }
+        #endregion
+
+        #region 微信支付
+        /// <summary>
+        /// 微信支付
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public object GetPay(string openid)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(openid))
+                    return Error("授权失败！");
+
+                var ip = HttpContext.Request.UserHostAddress;//获取ip
+                var orderno = Utils.GetOrderNumber();
+                var data = wxpay.JSAPIPayMent("测试", "测试", openid, ip, 0.01, orderno);
+                return Success(data);
+            }
+            catch (Exception ex)
+            {
+                return ErrorLog(ex.ToString(), "微信前端测试接口");
+            }
+
+        }
+        #endregion
+
+        #region 微信扫码支付
+        /// <summary>
+        /// 微信扫码支付
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public object GetScanPay()
+        {
+            try
+            {
+                var ip = HttpContext.Request.UserHostAddress;//获取ip
+                var orderno = Utils.GetOrderNumber();
+                var product_id = Guid.NewGuid().ToString("N");
+                var data = wxpay.NATIVEPayMent("测试", "测试", ip, 0.01, orderno, product_id);
+                return Success(data);
+            }
+            catch (Exception ex)
+            {
+                return ErrorLog(ex.ToString(), "微信前端测试接口");
+            }
+
+        }
+        #endregion
+
+        #region 根据openid获取基本信息
+        /// <summary>
+        /// 根据openid获取基本信息
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public object GetOpenidInformation(string openid)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(openid))
+                    return Error("授权失败！");
+                var data = wxpu.GetPerson(openid);
+                return Success(data);
+            }
+            catch (Exception ex)
+            {
+                return ErrorLog(ex.ToString(), "微信前端测试接口");
+            }
+
+        }
+        #endregion 
+
+    }
+}
--- webFront/API_V1.1/Wx_DockingController.cs
+++ webFront/API_V1.1/Wx_DockingController.cs
@@ -9,7 +9,7 @@ using WxTenpay.WXoperation.TModel;
 
 namespace webFront.API
 {
-    public class Wx_DockingController: BaseController
+    public class Wx_DockingController : BaseController
     {
         private WechatPublic wp = new WechatPublic();
         private Wx_EvenMessage wxevenmessage = new Wx_EvenMessage();
@@ -19,12 +19,32 @@ namespace webFront.API
             try
             {
                 GetConfig.ResetConfig();
-                var Eventlis = wxevenmessage.GetList().Where(x => x.WxAppId == WXconfig.appid);
-                var Messlis = wxmessage.GetList().Where(x => x.WxAppId == WXconfig.appid);
-                wp.SaveMesslis((List<Message>)Messlis, (List<EvenMessage>)Eventlis);
+                var Eventlis = wxevenmessage.GetList().Where(x => x.WxAppId == WXconfig.appid).Select(x =>
+                {
+                    return new EvenMessage
+                    {
+                      EventKey=  x.EventKey,
+                        EventType=  x.EventType,
+                        GuId= x.GuId,
+                    };
+                }).ToList();
+                var Messlis = wxmessage.GetList().Where(x => x.WxAppId == WXconfig.appid).Select(x =>
+                {
+                    return new Message
+                    {
+                        Content = x.Content,
+                        GuId = x.GuId,
+                        Key = x.Key,
+                        MatchingWay = x.MatchingWay,
+                        Name = x.Name,
+                        Type = x.Type,
+
+                    };
+                }).ToList();
+                wp.SaveMesslis(Messlis, Eventlis);
                 wp.WxDocking(WXconfig.Token);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 throw;
             }
